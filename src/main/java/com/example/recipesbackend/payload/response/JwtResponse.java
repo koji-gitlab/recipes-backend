36042f6a4ece341a266d1c5769e2700a5cff0bf0
koji <koji.gitlab@gmail.com>
2021-10-04 09:08:43 +0700

    Add JsonIgnore in JwtResponse class

--- src/main/java/com/example/recipesbackend/payload/response/JwtResponse.java
+++ src/main/java/com/example/recipesbackend/payload/response/JwtResponse.java
@@ -1,5 +1,6 @@
 package com.example.recipesbackend.payload.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import java.util.List;
@@ -11,10 +12,13 @@ public class JwtResponse {
 
     private String type = "Bearer";
 
+    @JsonIgnore
     private String id;
 
+    @JsonIgnore
     private String username;
 
+    @JsonIgnore
     private List<String> roles;
 
     public JwtResponse(String accessToken, String id, String username, List<String> roles) {
